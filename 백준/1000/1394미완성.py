b1435d77088d275f086de60b654a7b3aa6a485f7
JSC <gbs04087@gmail.com>
2021-12-08 04:49:47 +0900

    2206.py

--- 백준/1000/1394미완성.py
+++ 백준/1000/1394미완성.py
@@ -10,24 +10,37 @@ from itertools import permutations
 # str1 = sys.stdin.readline()
 # str2 = sys.stdin.readline()
 
-str1 = "abcdefghijklmnopqrstuvwxyz"
-str2 = "aa"
+# str1 = "abcdefghijklmnopqrstuvwxyz"
+# str2 = "ak"
 
 
-def fr(n, m):
-    return m * fr(n, m-1) if m > n else 1
+# def fr(n, m):
+#     return m * fr(n, m-1) if m > n else 1
 
 
-l = len(str1)
-result = 0
-for i in range(1, len(str2)+1):
-    if i < len(str2):
-        result += fr(l-i, l)
+# l = len(str1)
+# result = 0
+# for i in range(1, len(str2)+1):
+#     if i < len(str2):
+#         result += fr(l-i, l)
 
-print(permutations(str1, len(str2)))
-for k in permutations(str1, len(str2)):
-    result += 1
-    print("".join(k))
-    if "".join(k) == str2:
-        break
-print(result)
+# for k in permutations(str1, len(str2)):
+#     result += 1
+#     print("".join(k), str2)
+#     if "".join(k) == str2:
+#         break
+# print(result)
+
+import sys
+str1 = sys.stdin.readline().strip()
+str2 = sys.stdin.readline().strip()
+str1Len = len(str1)
+str2Len = len(str2)
+ans = str1Len**(str2Len-1)
+
+for i in range(str2Len):
+    ans += (str1.index(str2[i]))*(str1Len**(str2Len-i-1))
+if str1Len == 1:
+    print(str2Len)
+else:
+    print(((ans+1) % 900528))
--- 백준/2000/2206미완.py
+++ 백준/2000/2206미완.py
@@ -1,47 +1,40 @@
 # https://www.acmicpc.net/problem/2206
 
+# import time
+# import os
+# 주석풀면 움직이는게 보입니다??
+# for i in range(len(arr2)):
+#     print(arr1[i])
+# print(x1, y1, count)
+# time.sleep(0.3)
+# os.system('cls')
+
 
 import sys
 from collections import deque
-import time
-import os
-
 N, M = map(int, sys.stdin.readline().strip().split())
+arr1 = [list(map(int, list(sys.stdin.readline().strip()))) for _ in range(N)]
+arr2 = [[[-1, -1] for _ in range(M)] for _ in range(N)]
 
-arr1 = [sys.stdin.readline().strip() for i in range(N)]
-# print(arr1)
-
-arr2 = [[-1]*M for i in range(N)]
-# print(arr2)
-arr2[0][0] = 1
+arr2[0][0][0] = 1
 arrow = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 q = deque([(0, 0, 0)])
+flag = True
 while q:
-    x, y, count = q.popleft()
+    x1, y1, count = q.popleft()
+    if x1 == N-1 and y1 == M-1:
+        flag = False
+        print(arr2[N-1][M-1][count])
+        break
+    for x2, y2 in arrow:
+        x3, y3 = x1+x2, y1+y2
+        if 0 <= x3 < N and 0 <= y3 < M:
+            if arr1[x3][y3] == 1 and count == 0 and arr2[x3][y3][1] == -1:
+                arr2[x3][y3][1] = arr2[x1][y1][0] + 1
+                q.append((x3, y3, 1))
 
-    # 주석풀면 움직이는게 보입니다??
-    for i in range(len(arr2)):
-        print(arr2[i])
-    print(x, y, count)
-    time.sleep(0.3)
-    os.system('cls')
-
-    if x == N-1 and y == M-1:
-        continue
-    # if arr2[x][y] > 1:
-    #     continue
-
-    for xx, yy in arrow:
-        xxx, yyy = x+xx, y+yy
-        if 0 <= xxx < N and 0 <= yyy < M:
-            if arr2[xxx][yyy] < arr2[x][y] and count < 2:
-                if arr1[xxx][yyy] == '0':
-                    arr2[xxx][yyy] = arr2[x][y]+1
-                    q.appendleft((xxx, yyy, count))
-                elif arr1[xxx][yyy] == '1':
-                    arr2[xxx][yyy] = arr2[x][y]+1
-                    q.append((xxx, yyy, count+1))
-print(arr2[N-1][M-1])
-
-# for i in range(len(arr2)):
-#     print(arr2[i])
+            elif arr1[x3][y3] == 0 and arr2[x3][y3][count] == -1:
+                arr2[x3][y3][count] = arr2[x1][y1][count]+1
+                q.append((x3, y3, count))
+if flag:
+    print(-1)
