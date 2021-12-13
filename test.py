d38992f8c06390bed8cc05b68ac4a7fcc2127708
JSC <gbs04087@gmail.com>
2021-12-13 23:16:19 +0900

    test

--- test.py
+++ test.py
@@ -1,20 +1,26 @@
-import collections
+# import collections
 
 
-def removeDuplicateLetters(s: str) -> str:
-    counter, seen, stack = collections.Counter(s), set(), []
-    print(sorted(counter.items(), key=lambda x: x[0]))
+# def removeDuplicateLetters(s: str) -> str:
+#     counter, seen, stack = collections.Counter(s), set(), []
+#     print(sorted(counter.items(), key=lambda x: x[0]))
 
-    for char in s:
-        counter[char] -= 1
-        if char in seen:
-            continue
-        while stack and char < stack[-1] and counter[stack[-1]] > 0:
-            seen.remove(stack.pop())
-        stack.append(char)
-        seen.add(char)
+#     for char in s:
+#         counter[char] -= 1
+#         if char in seen:
+#             continue
+#         while stack and char < stack[-1] and counter[stack[-1]] > 0:
+#             seen.remove(stack.pop())
+#         stack.append(char)
+#         seen.add(char)
 
-    return ''.join(stack)
+#     return ''.join(stack)
 
 
-print(removeDuplicateLetters("asdsdsfqq"))
+# print(removeDuplicateLetters("asdsdsfqq"))
+
+# for k in range(1, 1000):
+#     if ((5300 + (9*k)) - (1000 + (2*k*k))) < 0:
+#         print("break", k)
+#         break
+#     print(5300 + (9*k), 1000 + (2*k*k), (300 + (9*k)) - (1000 + (2*k*k)))
