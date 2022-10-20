8f669bf52cc9d0be4a37cb261b20a2e632d5151f
JSC <gbs04087@gmail.com>
2022-10-20 16:20:12 +0900

    FEAT: 스타수열

---
+++ 프로그래머스/L3/스타수열.js
@@ -0,0 +1,33 @@
+function solution(a) {
+    const arr = new Array(10).fill(null).map(_=>[-1]);
+    for(let i = 0 ; i < a.length; i++){
+        arr[a[i]].push(i)
+    }
+    const check = (arr) =>{
+        let answer = 0;
+        for(let i of arr){
+            i.push(a.length);
+            let flag = true;
+            let count = 0;
+            console.log(i)
+            for(let j = 1 ; j < i.length-1; j++){
+                if(i[j]-i[j-1] > 1 && flag === true){
+                    console.log("-1",i[j]-i[j-1] )
+                    flag = true;
+                    count+=2;
+                }
+                else if(i[j+1]-i[j] > 1){
+                    console.log("-2",i[j+1]-i[j] )
+                    if(i[j+1]-i[j] === 2)
+                        flag = false;
+                    count+=2;
+                }
+                console.log(flag,count)
+            }
+            answer= Math.max(answer,count)
+            console.log(count)
+        }
+        return answer;
+    }
+    return check(arr);
+}
